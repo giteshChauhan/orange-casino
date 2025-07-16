6e1aebaed5bc73dc2859288baf953d45dd906684
Gitesh Chauhan <giteshchauhan20@gmail.com>
2025-07-16 13:30:53 +0530

    added slot config | symbols with their weight and payoutMultiplier

---
+++ backend/src/config/slot.config.ts
@@ -0,0 +1,7 @@
+export const symbolConfig = [
+  { symbol: '⭐', weight: 2, payoutMultiplier: 10 },
+  { symbol: '🍒', weight: 5, payoutMultiplier: 5 },
+  { symbol: '🔔', weight: 3, payoutMultiplier: 4 },
+  { symbol: '🍋', weight: 4, payoutMultiplier: 3 },
+  { symbol: '💎', weight: 1, payoutMultiplier: 15 },
+];
