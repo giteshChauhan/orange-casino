a8289a8de85c466693f12c1a72c3353f96527484
Gitesh Chauhan <giteshchauhan20@gmail.com>
2025-07-16 13:28:22 +0530

    updated jest config for unit testings

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,7 +1,15 @@
-import type { Config } from 'ts-jest';
-const config: Config = {
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.spec.ts']
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json'
+    }
+  }
 };
+
 export default config;
