25d5ccd4514274d6ec0f50289c0b79f7ecc9b830
Gitesh Chauhan <giteshchauhan20@gmail.com>
2025-07-16 19:37:37 +0530

    updated tournament model | added participants

--- backend/src/models/tournament.model.ts
+++ backend/src/models/tournament.model.ts
@@ -1,15 +1,21 @@
-import { Schema, model, Document } from 'mongoose';
-
+import { Schema, model, Document, Types } from 'mongoose';
 export interface TournamentDoc extends Document {
   name: string;
   startTime: Date;
   endTime: Date;
+  participants: Types.ObjectId[];
 }
 
 const tournamentSchema = new Schema<TournamentDoc>({
   name: { type: String, required: true },
   startTime: { type: Date, required: true },
-  endTime: { type: Date, required: true }
+  endTime: { type: Date, required: true },
+  participants: [
+    { type: Schema.Types.ObjectId, ref: 'User' }
+  ]
 });
 
-export const Tournament = model<TournamentDoc>('Tournament', tournamentSchema);
+export const Tournament = model<TournamentDoc>(
+  'Tournament',
+  tournamentSchema
+);
