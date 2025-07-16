57fd980b9fc104432d44b07596111003c3719527
Gitesh Chauhan <giteshchauhan20@gmail.com>
2025-07-16 19:36:49 +0530

    updated spin controller | added more socket events

--- backend/src/controllers/spin.controller.ts
+++ backend/src/controllers/spin.controller.ts
@@ -6,12 +6,9 @@ import { getIO } from '../utils/socket';
 export const spin = async (req: AuthRequest, res: Response) => {
   try {
     const { wager } = req.body;
-    const data = await playSpin(req.userId!, wager);
-
-    // Emit real-time update to this user
     const io = getIO();
+    const data = await playSpin(req.userId!, wager, io);
     io.to(req.userId!).emit('spin', data);
-
     res.json(data);
   } catch (err: any) {
     res.status(400).json({ message: err.message });
